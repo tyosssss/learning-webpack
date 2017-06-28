61d66c3a5ae8454022d9a74800a7dbe8a999d8f4
wangxiao <wangxiao@ctrip.com>
2017-06-28 20:07:02 +0800

    add files

--- webpack-2.6.1/lib/ParserHelpers.js
+++ webpack-2.6.1/lib/ParserHelpers.js
@@ -11,66 +11,96 @@ const UnsupportedFeatureWarning = require("./UnsupportedFeatureWarning");
 
 const ParserHelpers = exports;
 
-ParserHelpers.addParsedVariableToModule = function(parser, name, expression) {
-	if(!parser.state.current.addVariable) return false;
-	var deps = [];
-	parser.parse(expression, {
-		current: {
-			addDependency: function(dep) {
-				dep.userRequest = name;
-				deps.push(dep);
-			}
-		},
-		module: parser.state.module
-	});
-	parser.state.current.addVariable(name, expression, deps);
-	return true;
+ParserHelpers.addParsedVariableToModule = function (parser, name, expression) {
+  if (!parser.state.current.addVariable) return false;
+  var deps = [];
+  parser.parse(expression, {
+    current: {
+      addDependency: function (dep) {
+        dep.userRequest = name;
+        deps.push(dep);
+      }
+    },
+    module: parser.state.module
+  });
+  parser.state.current.addVariable(name, expression, deps);
+  return true;
 };
 
-ParserHelpers.requireFileAsExpression = function(context, pathToModule) {
-	var moduleJsPath = path.relative(context, pathToModule);
-	if(!/^[A-Z]:/i.test(moduleJsPath)) {
-		moduleJsPath = "./" + moduleJsPath.replace(/\\/g, "/");
-	}
-	return "require(" + JSON.stringify(moduleJsPath) + ")";
+
+ParserHelpers.requireFileAsExpression = function (context, pathToModule) {
+  var moduleJsPath = path.relative(context, pathToModule);
+  if (!/^[A-Z]:/i.test(moduleJsPath)) {
+    moduleJsPath = "./" + moduleJsPath.replace(/\\/g, "/");
+  }
+  return "require(" + JSON.stringify(moduleJsPath) + ")";
 };
 
-ParserHelpers.toConstantDependency = function(value) {
-	return function constDependency(expr) {
-		var dep = new ConstDependency(value, expr.range);
-		dep.loc = expr.loc;
-		this.state.current.addDependency(dep);
-		return true;
-	};
+/**
+ * 
+ * @param {String} value
+ * @returns {Boolean} 返回true , 表示后续插件fn无需执行
+ */
+ParserHelpers.toConstantDependency = function (value) {
+  return function constDependency(expr) {
+    var dep = new ConstDependency(value, expr.range);
+    dep.loc = expr.loc;
+    this.state.current.addDependency(dep);
+
+    return true;
+  };
 };
 
-ParserHelpers.evaluateToString = function(value) {
-	return function stringExpression(expr) {
-		return new BasicEvaluatedExpression().setString(value).setRange(expr.range);
-	};
+/**
+ * 
+ * @param {String} value
+ * @returns {Boolean}
+ */
+ParserHelpers.evaluateToString = function (value) {
+  return function stringExpression(expr) {
+    return new BasicEvaluatedExpression().setString(value).setRange(expr.range);
+  };
 };
 
-ParserHelpers.evaluateToBoolean = function(value) {
-	return function booleanExpression(expr) {
-		return new BasicEvaluatedExpression().setBoolean(value).setRange(expr.range);
-	};
+/**
+ * 
+ * @param {String} value
+ * @returns {Boolean}
+ */
+ParserHelpers.evaluateToBoolean = function (value) {
+  return function booleanExpression(expr) {
+    return new BasicEvaluatedExpression().setBoolean(value).setRange(expr.range);
+  };
 };
 
-ParserHelpers.expressionIsUnsupported = function(message) {
-	return function unsupportedExpression(expr) {
-		var dep = new ConstDependency("(void 0)", expr.range);
-		dep.loc = expr.loc;
-		this.state.current.addDependency(dep);
-		if(!this.state.module) return;
-		this.state.module.warnings.push(new UnsupportedFeatureWarning(this.state.module, message));
-		return true;
-	};
+/**
+ * 
+ * @param {String} value
+ * @returns {Boolean}
+ */
+ParserHelpers.expressionIsUnsupported = function (message) {
+  return function unsupportedExpression(expr) {
+    var dep = new ConstDependency("(void 0)", expr.range);
+    dep.loc = expr.loc;
+    this.state.current.addDependency(dep);
+    if (!this.state.module) return;
+    this.state.module.warnings.push(new UnsupportedFeatureWarning(this.state.module, message));
+    return true;
+  };
 };
 
+/**
+ * 
+ * @returns {Boolean}
+ */
 ParserHelpers.skipTraversal = function skipTraversal() {
-	return true;
+  return true;
 };
 
+/**
+ * 
+ * @returns {Boolean}
+ */
 ParserHelpers.approve = function approve() {
-	return true;
+  return true;
 };
--- webpack-2.6.1/lib/dependencies/CommonJsPlugin.js
+++ webpack-2.6.1/lib/dependencies/CommonJsPlugin.js
@@ -18,71 +18,91 @@ const CommonJsRequireDependencyParserPlugin = require("./CommonJsRequireDependen
 
 const ParserHelpers = require("../ParserHelpers");
 
+/**
+ * 处理 CommonJS 相关的 API
+ * 
+ * @class CommonJsPlugin
+ */
 class CommonJsPlugin {
-	constructor(options) {
-		this.options = options;
-	}
+  constructor(options) {
+    this.options = options;
+  }
 
-	apply(compiler) {
-		const options = this.options;
-		compiler.plugin("compilation", (compilation, params) => {
-			const normalModuleFactory = params.normalModuleFactory;
-			const contextModuleFactory = params.contextModuleFactory;
+  apply(compiler) {
+    const options = this.options;
 
-			compilation.dependencyFactories.set(CommonJsRequireDependency, normalModuleFactory);
-			compilation.dependencyTemplates.set(CommonJsRequireDependency, new CommonJsRequireDependency.Template());
+    compiler.plugin("compilation", (compilation, params) => {
+      /**
+       * 添加依赖工厂和依赖模板实例 ==> Compilation
+       */
+      const normalModuleFactory = params.normalModuleFactory;
+      const contextModuleFactory = params.contextModuleFactory;
 
-			compilation.dependencyFactories.set(CommonJsRequireContextDependency, contextModuleFactory);
-			compilation.dependencyTemplates.set(CommonJsRequireContextDependency, new CommonJsRequireContextDependency.Template());
+      compilation.dependencyFactories.set(CommonJsRequireDependency, normalModuleFactory);
+      compilation.dependencyTemplates.set(CommonJsRequireDependency, new CommonJsRequireDependency.Template());
 
-			compilation.dependencyFactories.set(RequireResolveDependency, normalModuleFactory);
-			compilation.dependencyTemplates.set(RequireResolveDependency, new RequireResolveDependency.Template());
+      compilation.dependencyFactories.set(CommonJsRequireContextDependency, contextModuleFactory);
+      compilation.dependencyTemplates.set(CommonJsRequireContextDependency, new CommonJsRequireContextDependency.Template());
 
-			compilation.dependencyFactories.set(RequireResolveContextDependency, contextModuleFactory);
-			compilation.dependencyTemplates.set(RequireResolveContextDependency, new RequireResolveContextDependency.Template());
+      compilation.dependencyFactories.set(RequireResolveDependency, normalModuleFactory);
+      compilation.dependencyTemplates.set(RequireResolveDependency, new RequireResolveDependency.Template());
 
-			compilation.dependencyFactories.set(RequireResolveHeaderDependency, new NullFactory());
-			compilation.dependencyTemplates.set(RequireResolveHeaderDependency, new RequireResolveHeaderDependency.Template());
+      compilation.dependencyFactories.set(RequireResolveContextDependency, contextModuleFactory);
+      compilation.dependencyTemplates.set(RequireResolveContextDependency, new RequireResolveContextDependency.Template());
 
-			compilation.dependencyFactories.set(RequireHeaderDependency, new NullFactory());
-			compilation.dependencyTemplates.set(RequireHeaderDependency, new RequireHeaderDependency.Template());
+      compilation.dependencyFactories.set(RequireResolveHeaderDependency, new NullFactory());
+      compilation.dependencyTemplates.set(RequireResolveHeaderDependency, new RequireResolveHeaderDependency.Template());
 
-			params.normalModuleFactory.plugin("parser", (parser, parserOptions) => {
+      compilation.dependencyFactories.set(RequireHeaderDependency, new NullFactory());
+      compilation.dependencyTemplates.set(RequireHeaderDependency, new RequireHeaderDependency.Template());
 
-				if(typeof parserOptions.commonjs !== "undefined" && !parserOptions.commonjs)
-					return;
 
-				const requireExpressions = ["require", "require.resolve", "require.resolveWeak"];
-				for(let expression of requireExpressions) {
-					parser.plugin(`typeof ${expression}`, ParserHelpers.toConstantDependency("function"));
-					parser.plugin(`evaluate typeof ${expression}`, ParserHelpers.evaluateToString("function"));
-				}
 
-				parser.plugin("evaluate typeof module", ParserHelpers.evaluateToString("object"));
-				parser.plugin("assign require", (expr) => {
-					// to not leak to global "require", we need to define a local require here.
-					const dep = new ConstDependency("var require;", 0);
-					dep.loc = expr.loc;
-					parser.state.current.addDependency(dep);
-					parser.scope.definitions.push("require");
-					return true;
-				});
-				parser.plugin("can-rename require", () => true);
-				parser.plugin("rename require", (expr) => {
-					// define the require variable. It's still undefined, but not "not defined".
-					const dep = new ConstDependency("var require;", 0);
-					dep.loc = expr.loc;
-					parser.state.current.addDependency(dep);
-					return false;
-				});
-				parser.plugin("typeof module", () => true);
-				parser.plugin("evaluate typeof exports", ParserHelpers.evaluateToString("object"));
-				parser.apply(
-					new CommonJsRequireDependencyParserPlugin(options),
-					new RequireResolveDependencyParserPlugin(options)
-				);
-			});
-		});
-	}
+      /**
+       * 绑定Parser事件 -- 解析代码
+       */
+      params.normalModuleFactory.plugin("parser", (parser, parserOptions) => {
+        if (typeof parserOptions.commonjs !== "undefined" && !parserOptions.commonjs) {
+          return;
+        }
+
+        const requireExpressions = ["require", "require.resolve", "require.resolveWeak"];
+        
+        for (let expression of requireExpressions) {
+          parser.plugin(`typeof ${expression}`, ParserHelpers.toConstantDependency("function"));
+          parser.plugin(`evaluate typeof ${expression}`, ParserHelpers.evaluateToString("function"));
+        }
+
+        parser.plugin("evaluate typeof module", ParserHelpers.evaluateToString("object"));
+        parser.plugin("assign require", (expr) => {
+          // to not leak to global "require", we need to define a local require here.
+          const dep = new ConstDependency("var require;", 0);
+          dep.loc = expr.loc;
+          parser.state.current.addDependency(dep);
+          parser.scope.definitions.push("require");
+          return true;
+        });
+        
+        parser.plugin("can-rename require", () => true);
+        
+        parser.plugin("rename require", (expr) => {
+          // define the require variable. It's still undefined, but not "not defined".
+          const dep = new ConstDependency("var require;", 0);
+          dep.loc = expr.loc;
+          parser.state.current.addDependency(dep);
+          return false;
+        });
+
+        parser.plugin("typeof module", () => true);
+        
+        parser.plugin("evaluate typeof exports", ParserHelpers.evaluateToString("object"));
+        
+        parser.apply(
+          new CommonJsRequireDependencyParserPlugin(options),
+          new RequireResolveDependencyParserPlugin(options)
+        );
+      });
+    });
+  }
 }
 module.exports = CommonJsPlugin;
